cd2946fecc63de1e26a64b7ce29b2dc3b163c476
sureshcskkumar <sureshcskkumar@gmail.com>
2022-04-18 18:29:34 +0530

    eureka server

---
+++ flightapp-eurekaserver/src/main/java/com/flightapp/flightappeurekaserver/FlightappEurekaserverApplication.java
@@ -0,0 +1,13 @@
+package com.flightapp.flightappeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightappEurekaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightappEurekaserverApplication.class, args);
+	}
+
+}
